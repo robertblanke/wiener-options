bb5fb029869af78071421e5ddb54832eabda34cc
Robert Blanke <Robert.Blanke95@live.com>
2025-09-05 14:40:02 +0200

    docs(api): add JavaDoc for OptionEvaluationDto

--- api/src/main/java/com/github/robertblanke/wiener_options_api/dto/OptionEvaluationDto.java
+++ api/src/main/java/com/github/robertblanke/wiener_options_api/dto/OptionEvaluationDto.java
@@ -1,4 +1,11 @@
 package com.github.robertblanke.wiener_options_api.dto;
 
+/**
+ * Data transfer object representing the evaluation of an option.
+ *
+ * @param simulatedOptionValue The option value estimated by simulation.
+ * @param wienerProcesses The simulated Wiener processes used in the evaluation.
+ * @param blackScholesOptionValue The option value calculated using the Black-Scholes model.
+ */
 public record OptionEvaluationDto(
     float simulatedOptionValue, float[][] wienerProcesses, float blackScholesOptionValue) {}
