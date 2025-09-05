a38caff912843e4ab5ace17c67ae4429f58478b2
Robert Blanke <Robert.Blanke95@live.com>
2025-09-05 18:33:45 +0200

    feat(api): implement DefaultOptionEvaluationService for option evaluation

---
+++ api/src/main/java/com/github/robertblanke/wiener_options_api/service/DefaultOptionEvaluationService.java
@@ -0,0 +1,18 @@
+package com.github.robertblanke.wiener_options_api.service;
+
+import com.github.robertblanke.wiener_options_api.dto.OptionEvaluationDto;
+import com.github.robertblanke.wiener_options_api.service.parameter.WienerProcessParameters;
+import org.springframework.stereotype.Service;
+
+/**
+ * Default implementation of the OptionEvaluationService interface.
+ */
+@Service
+public class DefaultOptionEvaluationService implements OptionEvaluationService {
+
+  @Override
+  public OptionEvaluationDto evaluate(
+      int numberOfPaths, WienerProcessParameters wienerProcessParameters) {
+    return null;
+  }
+}
