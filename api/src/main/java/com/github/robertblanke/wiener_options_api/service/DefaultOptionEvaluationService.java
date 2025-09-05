22b62efc763dd2a70a0389a747ed77247f42e757
Robert Blanke <Robert.Blanke95@live.com>
2025-09-05 18:34:14 +0200

    style(api): condense JavaDoc comment for DefaultOptionEvaluationService

--- api/src/main/java/com/github/robertblanke/wiener_options_api/service/DefaultOptionEvaluationService.java
+++ api/src/main/java/com/github/robertblanke/wiener_options_api/service/DefaultOptionEvaluationService.java
@@ -4,9 +4,7 @@ import com.github.robertblanke.wiener_options_api.dto.OptionEvaluationDto;
 import com.github.robertblanke.wiener_options_api.service.parameter.WienerProcessParameters;
 import org.springframework.stereotype.Service;
 
-/**
- * Default implementation of the OptionEvaluationService interface.
- */
+/** Default implementation of the OptionEvaluationService interface. */
 @Service
 public class DefaultOptionEvaluationService implements OptionEvaluationService {
 
