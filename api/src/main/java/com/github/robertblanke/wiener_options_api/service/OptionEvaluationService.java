13fde2132eef1c061dac67787b9c54552765f0dd
Robert Blanke <Robert.Blanke95@live.com>
2025-09-05 17:49:51 +0200

    feat(api): update evaluate method to use WienerProcessParameters

--- api/src/main/java/com/github/robertblanke/wiener_options_api/service/OptionEvaluationService.java
+++ api/src/main/java/com/github/robertblanke/wiener_options_api/service/OptionEvaluationService.java
@@ -1,6 +1,7 @@
 package com.github.robertblanke.wiener_options_api.service;
 
 import com.github.robertblanke.wiener_options_api.dto.OptionEvaluationDto;
+import com.github.robertblanke.wiener_options_api.service.parameter.WienerProcessParameters;
 
 /** Service interface for evaluating financial options. */
 public interface OptionEvaluationService {
@@ -9,18 +10,8 @@ public interface OptionEvaluationService {
    * Evaluates a financial option using simulation.
    *
    * @param numberOfPaths the number of simulation paths to use
-   * @param spotPrice the current price of the underlying asset
-   * @param riskFreeRate the risk-free interest rate
-   * @param assetVolatility the volatility of the underlying asset
-   * @param timeStep the time increment for each simulation step
-   * @param numberOfTimeSteps the total number of time steps in the simulation
+   * @param wienerProcessParameters the parameters for simulating the Wiener process
    * @return an {@link OptionEvaluationDto} containing the evaluation results
    */
-  OptionEvaluationDto evaluate(
-      int numberOfPaths,
-      float spotPrice,
-      float riskFreeRate,
-      float assetVolatility,
-      float timeStep,
-      int numberOfTimeSteps);
+  OptionEvaluationDto evaluate(int numberOfPaths, WienerProcessParameters wienerProcessParameters);
 }
