a92480f74a5c475940ed4f610ae0eef85ab4092c
Robert Blanke <Robert.Blanke95@live.com>
2025-09-05 14:43:50 +0200

    docs(api): remove periods from @param descriptions in OptionEvaluationDto

--- api/src/main/java/com/github/robertblanke/wiener_options_api/dto/OptionEvaluationDto.java
+++ api/src/main/java/com/github/robertblanke/wiener_options_api/dto/OptionEvaluationDto.java
@@ -3,9 +3,9 @@ package com.github.robertblanke.wiener_options_api.dto;
 /**
  * Data transfer object representing the evaluation of an option.
  *
- * @param simulatedOptionValue The option value estimated by simulation.
- * @param wienerProcesses The simulated Wiener processes used in the evaluation.
- * @param blackScholesOptionValue The option value calculated using the Black-Scholes model.
+ * @param simulatedOptionValue The option value estimated by simulation
+ * @param wienerProcesses The simulated Wiener processes used in the evaluation
+ * @param blackScholesOptionValue The option value calculated using the Black-Scholes model
  */
 public record OptionEvaluationDto(
     float simulatedOptionValue, float[][] wienerProcesses, float blackScholesOptionValue) {}
