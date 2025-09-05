6d3374be374790bce1e93677ce89bf8c443bd673
Robert Blanke <Robert.Blanke95@live.com>
2025-09-05 13:24:32 +0200

    feat(api): initialize Spring Boot project

---
+++ api/src/main/java/com/github/robertblanke/wiener_options_api/WienerOptionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.robertblanke.wiener_options_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WienerOptionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WienerOptionsApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/github/robertblanke/wiener_options_api/WienerOptionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.robertblanke.wiener_options_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WienerOptionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
